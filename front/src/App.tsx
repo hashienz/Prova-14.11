4a64cb2cb9466f838193b5278daee9abefb48998
hashienz <enzohashimoto57@gmail.com>
2025-11-04 10:59:41 -0300

    Revisao 2

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+//Componentes
+// - HTML, CSS e JS ou TS
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Listar Produtos</Link>
+            </li>
+            <li>
+              <Link to="/produto/cadastrar"> Cadastrar Produtos </Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          {/* <Route path="/" element={<ListarProdutos/>} />
+          <Route path="/produto/cadastrar" element={<CadastrarProduto/>} /> */}
+        </Routes>
+        <footer>
+          Rodapé da aplicação
+        </footer>
+      </BrowserRouter>
     </div>
   );
 }
